cc576d34b2c38cb268657d7d8aa6961302aa4122
taehhh8 <95203882+taehhh8@users.noreply.github.com>
2021-12-21 15:01:02 +0900

    tae
    tae

---
+++ hello-react/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import './InputExam';
+import InputExam from './InputExam';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <InputExam />
+      {/*<Block4 />*/}
+      {/* <h1> hello World </h1> */}
+    </div>
+  );
+}
+
+export default App;
