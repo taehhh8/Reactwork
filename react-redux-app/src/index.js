0ebe58c99b85ce77bec6d7d63c977502f5fc401e
taehhh8 <95203882+taehhh8@users.noreply.github.com>
2021-12-24 12:52:50 +0900

    tae
    tae

--- react-redux-app/src/index.js
+++ react-redux-app/src/index.js
@@ -7,15 +7,14 @@ import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux'; // Provider로 store를 넣어서 App 을 감싸게 되면 우리가 렌더링하는 그 어떤 컴포넌트던지 리덕스 스토어에 접근 할 수 있게 된답니다.
 import rootReducer from './modules/index';
+import { composeWithDevTools } from 'redux-devtools-extension'; // 리덕스 개발자 도구
 
-const store = createStore(rootReducer); // 스토어를 만듭니다.
+const store = createStore(rootReducer, composeWithDevTools()); // 스토어를 만듭니다.
 // console.log(store.getState()); // 스토어의 상태를 확인해봅시다.
 
 ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>
       <App />
-    </React.StrictMode>
   </Provider>,
   document.getElementById('root')
 );
